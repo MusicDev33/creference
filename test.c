0b593266a154ff50400f6e31f90d9cca0b36e597
MusicDev <smccowan33@gmail.com>
2023-07-09 12:45:25 -0700

    More content

--- test.c
+++ test.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void doTask(char *value) {
+// Pass in a pointer and increment the value of the pointer (but not the pointer itself)
+void passByRef(char *value) {
   ++*value;
 }
 
 int main() {
   short age = 40;
   char name[7] = "test";
-  doTask(&(*name));
+  passByRef(&(*name));
+
+  // Compiler optimizes this away and makes these allocate to the stack
   char *str;
   str = (char *) malloc(15);
   str = (char *) realloc(str, 25);
@@ -16,8 +19,11 @@ int main() {
   short *addr = &age;
 
   printf("%p\n", &age);
+
+  // Show the result of incrementing letters in the name
   printf("%s\n", name);
   printf("%u\n", *(name + 1));
+
   printf("%p\n", &str);
   printf("%u", *addr);
 }
